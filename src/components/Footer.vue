76b41e3e2f2a4a72f53d75c2d30f763dd62bf2fc
Suradet P150 <pharmacistsabot@gmail.com>
2025-12-03 14:04:41 +0700

    Test fatal error

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,13 +1,13 @@
 <template>
-  <footer class="app-footer">
-    <div class="container">
-      <p class="footer-reference">อ้างอิงตามประกาศ สปสช. วันที่ 10 เมษายน 2568</p>
-      <p class="footer-copyright">
-        <!-- ใช้ Interpolation {{ }} เพื่อแสดงค่าตัวแปร -->
-        &copy; {{ currentYear }} ลิขสิทธิ์ | พัฒนาโดย ภก. สุรเดช ประถมศักดิ์
-      </p>
-    </div>
-  </footer>
+    <footer class="app-footer">
+        <div class="container">
+            <p class="footer-reference">อ้างอิงตามประกาศ สปสช. วันที่ 10 เมษายน 2568</p>
+            <p class="footer-copyright">
+                <!-- ใช้ Interpolation {{ }} เพื่อแสดงค่าตัวแปร -->
+                &copy; {{ currentYear }} ลิขสิทธิ์ | พัฒนาโดย ภก. สุรเดช ประถมศักดิ์
+            </p>
+        </div>
+    </footer>
 </template>
 
 <script setup lang="ts">
@@ -16,21 +16,21 @@ const currentYear: number = new Date().getFullYear();
 
 <style scoped>
 .app-footer {
-  background-color: var(--dark-color);
-  color: white;
-  padding: 20px 0;
-  text-align: center;
-  margin-top: auto;
+    background-color: var(--dark-color);
+    color: white;
+    padding: 20px 0;
+    text-align: center;
+    margin-top: auto;
 }
 
 .footer-reference {
-  font-size: 0.9rem;
-  opacity: 0.8;
-  margin-bottom: 5px;
+    font-size: 0.9rem;
+    opacity: 0.8;
+    margin-bottom: 5px;
 }
 
 .footer-copyright {
-  font-size: 0.9rem;
-  font-weight: 500;
+    font-size: 0.9rem;
+    font-weight: 500;
 }
 </style>
--- src/main.ts
+++ src/main.ts
@@ -4,5 +4,6 @@ import router from './router';
 import './assets/main.css';
 
 const app = createApp(App);
+const secret = process.env.MY_SECRET;
 app.use(router);
 app.mount('#app');
